76325799be2c8e9621ae05bd9553b61c63bbc057
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 03:22:43 -0400

    last one

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -8,4 +8,4 @@ git add -f docs/*
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit --allow-empty -m "${COMMIT_MESSAGE}"
 git remote add origin-pages "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
-git push origin-pages gh-pages
+git push --set-upstream origin-pages gh-pages
