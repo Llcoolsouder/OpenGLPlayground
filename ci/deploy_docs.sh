e805f446ec6ae03fd6b0db07b3a23e5a84d2f1cd
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 02:35:36 -0400

    still trying to fix travis commits

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -5,7 +5,8 @@ doxygen Doxyfile
 
 # Commit changes and push to origin
 git add -f docs/*
-COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER}"
+COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit -m "$COMMIT_MESSAGE"
-git remote add origin-travis "https://${GH_TOKEN}@github.com/Llcoolsouder/OpenGLPlayground.git" > /dev/null 2>&1
-git push --quiet --set-upstream --force origin-travis master
+git remote rm origin
+git remote add origin "https://llcoolsouder:${GH_TOKEN}@github.com/Llcoolsouder/OpenGLPlayground.git" > /dev/null 2>&1
+git push --quiet --force origin master
