3ff561bc84159533a68873488b474afead026f5d
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 01:53:09 -0400

    More travis fixes

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -4,8 +4,8 @@
 doxygen Doxyfile
 
 # Commit changes and push to origin
-git add -f docs/
-COMMIT_MESSAGE= "Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER}"
-git commit -m $COMMIT_MESSAGE
+git add -f docs/*
+COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER}"
+git commit -m "$COMMIT_MESSAGE"
 REPO="https://llcoolsouder:${GH_TOKEN}@github.com/Llcoolsouder/OpenGLPlayground.git"
 git push $REPO master
