ef3b7786e682a357ab59c06cb853a462305716f5
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 02:22:31 -0400

    push harder

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -8,4 +8,4 @@ git add -f docs/*
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER}"
 git commit -m "$COMMIT_MESSAGE"
 git remote add origin-travis "https://${GH_TOKEN}@github.com/Llcoolsouder/OpenGLPlayground.git" > /dev/null 2>&1
-git push --quiet origin-travis master
+git push --quiet --set-upstream --force origin-travis master
