46f15be4eaa7e39b61f4e06bf6fffc0572dcd0d6
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 03:07:45 -0400

    debugging

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -7,4 +7,5 @@ doxygen Doxyfile
 git add -f docs/*
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit --allow-empty -m "${COMMIT_MESSAGE}"
+echo "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
 git push --quiet --force "https://llcoolsouder:${GH_TOKEN}@${GH_REF}" HEAD:master > /dev/null 2>&1
