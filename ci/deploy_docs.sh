6c749f2896b8d409dad4362e375ba504c83db11d
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 10:58:19 -0400

    more info

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -2,11 +2,15 @@
 
 # Build docs
 doxygen Doxyfile
+ls -lh
+ls docs -lh
 
 # Commit changes and push to origin
+git status
 git add -f docs/*
+git status
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
-git commit --allow-empty -m "${COMMIT_MESSAGE}"
+git commit -m "${COMMIT_MESSAGE}"
 git status
 git remote add origin-travis "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
 git push --set-upstream origin-travis master
