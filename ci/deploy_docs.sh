837117686e99f35ec81732bbe18690a29dd1739a
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 10:22:35 -0400

    trying travis gem encrypt

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -7,5 +7,5 @@ doxygen Doxyfile
 git add -f docs/*
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit --allow-empty -m "${COMMIT_MESSAGE}"
-git remote add origin-pages "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
-git push --set-upstream origin-pages gh-pages
+git remote add origin-travis "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
+git push --set-upstream origin-travis master
