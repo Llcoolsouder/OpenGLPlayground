e8a2e8c0b69bc772b4ad6cb571ae95ad8f1c2d74
Lonnie Souder II <llcoolsouder@souderfamily.net>
2019-08-17 12:24:57 -0400

    Cleanup status in deploy script and adding to buid matrix

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Get out of detached HEAD state
 git checkout master
 
 # Build docs
@@ -9,11 +10,8 @@ cp docs/html/* docs/
 rm -rf docs/html
 
 # Commit changes and push to origin
-git status
 git add -f docs/*
-git status
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit -m "${COMMIT_MESSAGE}"
-git status
 git remote add origin-travis "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
 git push --set-upstream origin-travis master
