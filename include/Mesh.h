22d9d498c9f7d75f8c12b67909c8d90b7f616c3e
Lonnie Souder <souderl9@students.rowan.edu>
2019-09-08 13:16:25 -0400

    Seems like i missed these when adding clang-format. Still needs a fix for the doxygen comments

--- include/Mesh.h
+++ include/Mesh.h
@@ -21,46 +21,45 @@
 class Mesh
 {
 public:
-  Mesh() = delete;
-  Mesh(const Mesh& other) = delete;
+	Mesh() = delete;
+	Mesh(const Mesh &other) = delete;
 
-  /**
+	/**
    * Creates a mesh with one interleaved vertex buffer
    */
-  Mesh(
-    std::vector<glm::vec3> aPositions,
-    std::vector<unsigned int> Indices,
-    std::vector<glm::vec4> aColors = {},
-    GLenum DrawMode = GL_TRIANGLES);
+	Mesh(
+		std::vector<glm::vec3> aPositions,
+		std::vector<unsigned int> Indices,
+		std::vector<glm::vec4> aColors = {},
+		GLenum DrawMode = GL_TRIANGLES);
 
-  /**
+	/**
    * Destroys all buffer objects in this mesh
    */
-  ~Mesh();
+	~Mesh();
 
-  /**
+	/**
    * Binds the mesh buffers to the context
    */
-  void Bind() const;
+	void Bind() const;
 
-  /**
+	/**
    * Draws the mesh using the currently bound shader
    */
-  void Draw() const;
+	void Draw() const;
 
 private:
+	void PushPosition(std::vector<float> &, const glm::vec3 &) const;
 
-  void PushPosition(std::vector<float>&, const glm::vec3&) const;
-
-  void PushColor(std::vector<float>&, const glm::vec4&) const;
+	void PushColor(std::vector<float> &, const glm::vec4 &) const;
 
 private:
-  GLuint mVAO;
-  GLuint mVertexBufferID;
-  GLuint mElementBufferID;
-  GLenum mDrawMode;
+	GLuint mVAO;
+	GLuint mVertexBufferID;
+	GLuint mElementBufferID;
+	GLenum mDrawMode;
 
-  // TODO: These aren't strictly necessary at the moment. Revisit this later on. 08/29/19
-  unsigned int mNumVertices;
-  unsigned int mVertexSize;
+	// TODO: These aren't strictly necessary at the moment. Revisit this later on. 08/29/19
+	unsigned int mNumVertices;
+	unsigned int mVertexSize;
 };
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -10,81 +10,81 @@
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 Mesh::Mesh(std::vector<glm::vec3> aPositions,
-  std::vector<unsigned int> Indices,
-  std::vector<glm::vec4> aColors,
-  GLenum DrawMode)
-  : mVAO(0),
-  mVertexBufferID(0),
-  mElementBufferID(0),
-  mDrawMode(DrawMode),
-  mNumVertices(0),
-  mVertexSize(0)
+	std::vector<unsigned int> Indices,
+	std::vector<glm::vec4> aColors,
+	GLenum DrawMode) :
+		mVAO(0),
+		mVertexBufferID(0),
+		mElementBufferID(0),
+		mDrawMode(DrawMode),
+		mNumVertices(0),
+		mVertexSize(0)
 {
-  // TODO: Find a better way to handle data for different shaders/meshs
-  // TODO: Write/find better error handling code
-  assert(aColors.empty() || (aPositions.size() == aColors.size()));
-  assert(!Indices.empty());
+	// TODO: Find a better way to handle data for different shaders/meshs
+	// TODO: Write/find better error handling code
+	assert(aColors.empty() || (aPositions.size() == aColors.size()));
+	assert(!Indices.empty());
 
-  // Determine size of vertex
-  if (!aPositions.empty()) mVertexSize += sizeof(aPositions[0]);
-  if (!aColors.empty()) mVertexSize += sizeof(aColors[0]);
+	// Determine size of vertex
+	if (!aPositions.empty()) mVertexSize += sizeof(aPositions[0]);
+	if (!aColors.empty()) mVertexSize += sizeof(aColors[0]);
 
-  // Determine number of vertices
-  mNumVertices = aPositions.size();
+	// Determine number of vertices
+	mNumVertices = aPositions.size();
 
-  // Create interleaved buffer
-  std::vector<float> VertexBuffer;
-  for (int i = 0; i < mNumVertices; i++)
-  {
-    PushPosition(VertexBuffer, aPositions[i]);
+	// Create interleaved buffer
+	std::vector<float> VertexBuffer;
+	for (int i = 0; i < mNumVertices; i++)
+	{
+		PushPosition(VertexBuffer, aPositions[i]);
 
-    if (!aColors.empty())
-    {
-      PushColor(VertexBuffer, aColors[i]);
-    }
-  }
+		if (!aColors.empty())
+		{
+			PushColor(VertexBuffer, aColors[i]);
+		}
+	}
 
-  // Generate VertexArray and buffers
-  glGenVertexArrays(1, &mVAO);
-  glGenBuffers(1, &mVertexBufferID);
-  glGenBuffers(1, &mElementBufferID);
+	// Generate VertexArray and buffers
+	glGenVertexArrays(1, &mVAO);
+	glGenBuffers(1, &mVertexBufferID);
+	glGenBuffers(1, &mElementBufferID);
 
-  // Configure buffers
-  glBindVertexArray(mVAO);
-  glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
-  glBufferData(GL_ARRAY_BUFFER,
-    VertexBuffer.size() * sizeof(VertexBuffer[0]),
-    VertexBuffer.data(),
-    GL_STATIC_DRAW);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElementBufferID);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-    Indices.size() * sizeof(Indices[0]),
-    Indices.data(),
-    GL_STATIC_DRAW);
-  glBindVertexArray(0);
+	// Configure buffers
+	glBindVertexArray(mVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER,
+		VertexBuffer.size() * sizeof(VertexBuffer[0]),
+		VertexBuffer.data(),
+		GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElementBufferID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		Indices.size() * sizeof(Indices[0]),
+		Indices.data(),
+		GL_STATIC_DRAW);
+	glBindVertexArray(0);
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 Mesh::~Mesh()
 {
-  glDeleteVertexArrays(1, &mVAO);
-  glDeleteBuffers(1, &mVertexBufferID);
-  glDeleteBuffers(1, &mElementBufferID);
+	glDeleteVertexArrays(1, &mVAO);
+	glDeleteBuffers(1, &mVertexBufferID);
+	glDeleteBuffers(1, &mElementBufferID);
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void Mesh::Bind() const
 {
-  glBindVertexArray(mVAO);
+	glBindVertexArray(mVAO);
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void Mesh::Draw() const
 {
-  glDrawElements(mDrawMode, mNumVertices, GL_UNSIGNED_INT, (void*)0);
+	glDrawElements(mDrawMode, mNumVertices, GL_UNSIGNED_INT, (void *)0);
 }
 
 //=============================================================================
@@ -92,22 +92,22 @@ void Mesh::Draw() const
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void Mesh::PushPosition(
-  std::vector<float>& VertexBuffer,
-  const glm::vec3& pos) const
+	std::vector<float> &VertexBuffer,
+	const glm::vec3 &pos) const
 {
-  VertexBuffer.push_back(static_cast<float>(pos.x));
-  VertexBuffer.push_back(static_cast<float>(pos.y));
-  VertexBuffer.push_back(static_cast<float>(pos.z));
+	VertexBuffer.push_back(static_cast<float>(pos.x));
+	VertexBuffer.push_back(static_cast<float>(pos.y));
+	VertexBuffer.push_back(static_cast<float>(pos.z));
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void Mesh::PushColor(
-  std::vector<float>& VertexBuffer,
-  const glm::vec4& color) const
+	std::vector<float> &VertexBuffer,
+	const glm::vec4 &color) const
 {
-  VertexBuffer.push_back(static_cast<float>(color.r));
-  VertexBuffer.push_back(static_cast<float>(color.g));
-  VertexBuffer.push_back(static_cast<float>(color.b));
-  VertexBuffer.push_back(static_cast<float>(color.a));
+	VertexBuffer.push_back(static_cast<float>(color.r));
+	VertexBuffer.push_back(static_cast<float>(color.g));
+	VertexBuffer.push_back(static_cast<float>(color.b));
+	VertexBuffer.push_back(static_cast<float>(color.a));
 }
