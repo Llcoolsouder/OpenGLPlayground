dba2a0268e3743cbde8d81391d44d97cf07c19bb
Lonnie Souder <souderl9@students.rowan.edu>
2019-09-15 12:30:40 -0400

    adding lcov install

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# Install lcov
+git clone https://github.com/linux-test-project/lcov.git
+cd lcov
+git checkout v1.14
+make install
+cd ..
+
+# Generate report
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
 lcov --list coverage.info
 bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+read -p "Press [Enter] key to start backup..."
