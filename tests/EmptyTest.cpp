6beab64908c470e79e8e33628f6e6b99a1befc03
Lonnie Souder II <llcoolsouder@souderfamily.net>
2020-02-13 20:43:15 -0500

    added camera to a test (playing with codecov)

--- tests/EmptyTest.cpp
+++ tests/EmptyTest.cpp
@@ -1,7 +1,10 @@
 // This is just a placeholder for CTests.
 // Need to add real tests for each object in OpenGLPlayground.
 
+#include <../include/Camera.h>
+
 int main()
 {
+	Camera camera;
 	return 0;
 }
