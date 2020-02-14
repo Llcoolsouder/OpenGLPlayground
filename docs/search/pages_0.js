180f2379067efb68628203ef0c4e982ffeada56c
Travis CI User <travis@example.org>
2020-02-14 18:37:10 +0000

    Travis: Updating docs for build #70 [skip ci]

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['opengl_20playground',['OpenGL Playground',['../index.html',1,'']]],
-  ['opengl_20playground_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fllcoolsouder_2fopenglplayground_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fllcoolsouder_2fopenglplayground_29',['OpenGL Playground [![Build Status](https://travis-ci.org/Llcoolsouder/OpenGLPlayground.svg?branch=master)](https://travis-ci.org/Llcoolsouder/OpenGLPlayground)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['opengl_20playground_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fllcoolsouder_2fopenglplayground_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fllcoolsouder_2fopenglplayground_29_20_5b_21_5bcodecov_5d_28https_3a_2f_2fcodecov_2eio_2fgh_2fllcoolsouder_2fopenglplayground_2fbranch_2fmaster_2fgraph_2fbadge_2esvg_29_5d_28https_3a_2f_2fcodecov_2eio_2fgh_2fllcoolsouder_2fopenglplayground_29',['OpenGL Playground [![Build Status](https://travis-ci.org/Llcoolsouder/OpenGLPlayground.svg?branch=master)](https://travis-ci.org/Llcoolsouder/OpenGLPlayground) [![codecov](https://codecov.io/gh/Llcoolsouder/OpenGLPlayground/branch/master/graph/badge.svg)](https://codecov.io/gh/Llcoolsouder/OpenGLPlayground)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
