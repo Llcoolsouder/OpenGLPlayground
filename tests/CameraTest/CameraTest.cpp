a22f16c176cde7aed17f0a05e3c9f819e204cd67
Lonnie Souder II <llcoolsouder@souderfamily.net>
2020-02-15 13:49:21 -0500

    Restructured test directory

---
+++ tests/CameraTest/CameraTest.cpp
@@ -0,0 +1,18 @@
+// This is just a placeholder for CTests.
+// Need to add real tests for each object in OpenGLPlayground.
+
+#include <glm/glm.hpp>
+#include <../include/Camera.h>
+
+int main()
+{
+	const glm::mat4 IdentityMatrix = glm::mat4(1.0f);
+	Camera camera;
+
+	if (*camera.GetViewMatrix() != IdentityMatrix)
+		return 1;
+	if (*camera.GetProjMatrix() != IdentityMatrix)
+		return 1;
+
+	return 0;
+}
