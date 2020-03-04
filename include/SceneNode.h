13b815f86ab956c8710bd386428c1d73a6fb339e
Lonnie Souder II <lonnie2@souderfamily.net>
2020-03-03 19:42:43 -0500

    Implemented basic scene node

--- include/SceneNode.h
+++ include/SceneNode.h
@@ -1,10 +1,10 @@
 /**
  * @file SceneNode.h
  *
- * Describes interface for a node in the Scene Graph
+ * Describes a node in the Scene Graph
  *
  * @author Lonnie L. Souder II
- * @date 09/07/2019
+ * @date 02/29/2020
  */
 #pragma once
 
@@ -14,16 +14,41 @@
 class SceneNode
 {
 public:
+	
+	SceneNode();
+
+	~SceneNode();
+
 	/**
 	 * Should handle all updates of the node
 	 * from one frame to the next
 	 */
-	virtual void Update() = 0;
+	virtual void Update();
 
 	/**
 	 * Should handle how the node is drawn
 	 */
-	virtual void Draw() const = 0;
+	virtual void Draw() const;
+
+	/**
+	 * Sets the pointer to the parent node
+	 * @param pNewParentNode Pointer to the new parent node
+	 */
+	virtual void SetParentNode(std::shared_ptr<SceneNode> pNewParentNode);
+
+	/**
+	 * Adds a child node to this node
+	 * @param pNewChildNode Pointer to the new child node to be added
+	 * @return true if pNewChildNode is successfully added else false
+	 */
+	virtual bool AddChildNode(std::shared_ptr<SceneNode> pNewChildNode);
+
+	/**
+	 * Removes a child node from this node
+	 * @param Pointer to the child node to be removed
+	 * @return true if pChildNode is successfully removed else false
+	 */
+	virtual bool RemoveChildNode(std::shared_ptr<SceneNode> pChildNode);
 
 protected:
 
---
+++ src/SceneNode.cpp
@@ -0,0 +1,72 @@
+#include "SceneNode.h"
+
+#include <algorithm>
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+SceneNode::SceneNode() :
+		mpParent(nullptr),
+		mChildren({})
+{
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+SceneNode::~SceneNode()
+{
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void SceneNode::Update()
+{
+	std::for_each(
+		mChildren.begin(),
+		mChildren.end(),
+		[&](auto pChild) { pChild->Update(); });
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void SceneNode::Draw() const
+{
+	std::for_each(
+		mChildren.begin(),
+		mChildren.end(),
+		[&](auto pChild) { pChild->Draw(); });
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+void SceneNode::SetParentNode(std::shared_ptr<SceneNode> pNewParentNode)
+{
+	mpParent = pNewParentNode;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool SceneNode::AddChildNode(std::shared_ptr<SceneNode> pNewChildNode)
+{
+	for (auto it = mChildren.begin(); it != mChildren.end(); ++it)
+	{
+		if (*it == pNewChildNode)
+			return false;
+	}
+	mChildren.push_back(pNewChildNode);
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool SceneNode::RemoveChildNode(std::shared_ptr<SceneNode> pChildNode)
+{
+	for (auto it = mChildren.begin(); it != mChildren.end(); ++it)
+	{
+		if (*it == pChildNode)
+		{
+			mChildren.erase(it);
+			return true;
+		}
+	}
+	return false;
+}
