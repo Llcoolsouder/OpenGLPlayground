9dbffa398a0c3e436e998c65ae81f7fe2331eb23
Lonnie Souder II <lonnie2@souderfamily.net>
2020-03-12 20:53:36 -0400

    changed remote url in travis script

--- ci/commit_changes.sh
+++ ci/commit_changes.sh
@@ -4,5 +4,5 @@
 git add -f TODO.md docs/*
 COMMIT_MESSAGE="Travis: Updating docs for build #${TRAVIS_BUILD_NUMBER} [skip ci]"
 git commit -m "${COMMIT_MESSAGE}"
-git remote add origin-travis "https://llcoolsouder:${GH_TOKEN}@${GH_REF}"
+git remote add origin-travis "https://${GH_TOKEN}@${GH_REF}"
 git push --set-upstream origin-travis master
