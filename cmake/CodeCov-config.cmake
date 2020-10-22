a18637fcc631653b6a1fa4b474317b67fb5a79ba
Lonnie Souder II <llcoolsouder@souderfamily.net>
2020-10-22 17:12:49 -0400

    cleaned up warnings in CodeCov

--- cmake/CodeCov-config.cmake
+++ cmake/CodeCov-config.cmake
@@ -1,7 +1,12 @@
 add_library(coverage_config INTERFACE)
 
 option(CODE_COVERAGE "Enable coverage reporting" OFF)
-if(CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+set(
+  USE_CODE_COVERAGE
+  $<AND:$<BOOL:${CODE_COVERAGE}>, $<IN_LIST:CMAKE_CXX_COMPILER_ID,"GNU;Clang">>)
+
+if(USE_CODE_COVERAGE)
+  message("Using CodeCov.")
   # Add required flags (GCC & LLVM/Clang)
   target_compile_options(coverage_config INTERFACE
     -O0        # no optimization
@@ -12,5 +17,5 @@ if(CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     target_link_options(coverage_config INTERFACE --coverage)
   else()
     target_link_libraries(coverage_config INTERFACE --coverage)
-  endif()
-endif(CODE_COVERAGE)
+  endif(CMAKE_VERSION VERSION_GREATER_EQUAL 3.13)
+endif(USE_CODE_COVERAGE)
