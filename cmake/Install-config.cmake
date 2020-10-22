08dafbca11c02ea16cc702d816e7d483dd8cce6d
Lonnie Souder II <llcoolsouder@souderfamily.net>
2020-10-22 10:27:59 -0400

    syntax in string

--- cmake/Install-config.cmake
+++ cmake/Install-config.cmake
@@ -7,7 +7,7 @@ if (PACKAGE_DEPENDENCIES)
         PROPERTY IMPORTED_LOCATION)
     message("OpenGL libs are at ${OPENGL_LIBS}")
     message("GLEW libs are at ${GLEW_SHARED_LIBRARIES}")
-    message("GLFW is at $GLFW3_LIBRARY")
+    message("GLFW is at ${GLFW3_LIBRARY}")
     install(
         FILES 
         DESTINATION lib)
